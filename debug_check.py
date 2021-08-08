3bc74007414d8bd8d4e80b76cd5d5ab3f7831cba
gurkan <gurkangnr1@gmail.com>
2021-08-08 16:40:23 +0300

    ***

--- debug_check.py
+++ debug_check.py
@@ -1,26 +1,11 @@
-def prime_factors(num):
-    factors = []
-    prime_numbers = []
-    num12 = num
-    i = 2
-    for i in range(2,num):
-        if num % i == 0:
-            factors.append(i)
-            num /= i
-            while num % i == 0:
-                factors.append(i)
-                num /= i
-                if num % i != 0: break
-    for j in range(2,int(num12)):
-        count = 0
-        for k in range(2,j+1):
-            if j % k == 0:
-                count += 1
-        if not count >= 2:
-            prime_numbers.append(j)
-    for m in factors:
-        if m not in prime_numbers:
-            factors.remove(m)
-    return num12
+def num_split(num):
+    digits = (list(str(num)))
+    number_of_digits = len(digits) - 1 
+    output = []
+    for i in digits:
+        x = i*10**number_of_digits
+        number_of_digits -= 1
+        output.append(x)
+    return output
 
-prime_factors(128000)
+num_split(39)
