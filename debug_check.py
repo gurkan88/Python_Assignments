3de314b7b37731c4c9388a6f8e67f87fbcaeef21
gurkan <gurkangnr1@gmail.com>
2021-08-11 14:36:09 +0300

    Assignment-7 FizzBuzz

--- debug_check.py
+++ debug_check.py
@@ -1,11 +1,12 @@
-def num_split(num):
-    digits = (list(str(num)))
-    number_of_digits = len(digits) - 1 
-    output = []
-    for i in digits:
-        x = i*10**number_of_digits
-        number_of_digits -= 1
-        output.append(x)
-    return output
 
-num_split(39)
+def freed_prisoners(prison):
+  if prison[0] == 0:
+      return 0
+  count = 1
+  for i in range(1, len(prison)):
+      if prison[i] != prison[i-1]:
+          count += 1
+  return count
+
+list1 = [1, 1, 0, 0, 0, 1, 0]
+freed_prisoners(list1)
