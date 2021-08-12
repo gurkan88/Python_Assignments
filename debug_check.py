5b13e3c07f8e08b0e73b7ccc422ac27a8be22e19
gurkan <gurkangnr1@gmail.com>
2021-08-12 14:48:59 +0300

    edabit challenge

--- debug_check.py
+++ debug_check.py
@@ -1,12 +1,11 @@
-
-def freed_prisoners(prison):
-  if prison[0] == 0:
-      return 0
-  count = 1
-  for i in range(1, len(prison)):
-      if prison[i] != prison[i-1]:
-          count += 1
-  return count
-
-list1 = [1, 1, 0, 0, 0, 1, 0]
-freed_prisoners(list1)
+def snakefill(n):
+    area = n*n - 1
+    count = 0
+    snake_len = 0
+    for i in range(area):
+        snake_len += 2**i
+        if snake_len < area :
+            count += 1
+    return count
+        
+snakefill(5)
