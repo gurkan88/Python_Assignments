04423ffd6029d41d530b72c6393084364e52884e
gurkan <gurkangnr1@gmail.com>
2021-08-13 13:47:49 +0300

    the least common multiple edabit challenge

--- debug_check.py
+++ debug_check.py
@@ -1,8 +1,27 @@
-def nextto(arg):
+def lcm(nums):
+    lcm1 = dict()
+    factors = []
+    result = 1
+    for i in nums :
+        key = i
+        lcm1.update({i : []})
+        for j in range(2,i) :
+            if i % j == 0 :
+                i /= j
+                lcm1[key].append(j)
+                while i % j == 0 :
+                    i /= j
+                    lcm1[key].append(j)
+    for k in lcm1.values() :
+        factors += k 
+    factors = set(factors)
 
-    if arg[arg.index("Arif")+1] or arg[arg.index("Arif")-1] == arg[arg.index("Raife")] :
-        return True
-    else: False
+    for k in factors :
+        number_of_factors = list(map(lambda x : x.count(k), lcm1.values()))
+        result *= k ** max(number_of_factors)
 
-print(nextto(["Arif","Nihal","Raife"]))
-print(nextto(["Arif","Raife","Nihal"]))
+    return result
+            
+
+v = [5,5,5]
+lcm(v)
