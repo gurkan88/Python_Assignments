b7c188d0c209c446162d7171244ef71769e6e0c9
gurkan <gurkangnr1@gmail.com>
2021-08-19 19:04:12 +0300

    edabit challenge

--- debug_check.py
+++ debug_check.py
@@ -1,30 +1,21 @@
-def permutations(list1):
-    factorial = 1
-    for i in range(1,len(list1)+1):
-        factorial *= i 
-    num_of_rows = int(factorial / len(list1))
-    output = []
-    for i in range(factorial):
-        output.append([])
+def multiply_by_11(n):
+    x = "0"
+    result = ""
+    n1 = "".join(("0",n))
+    n2 = n + "0"
+    for i,j in zip(n1[::-1],n2[::-1]) :
+        if int(i) + int(j) + int(x) < 10 :
+            result = "".join((str(int(i) + int(j) + int(x)), result))
+            x = "0"
+        else :
+            sum1 = int(i) + int(j) + int(x)
+            result = "".join((str(sum1)[-1], result))
+            x = f"{sum1 // 10}"
+    if x != 0 or x != "0":
+        result = "".join((str(x), result))
+    return result  
 
-    index1 = 0
-    y = 0
-    u = 0
-    w = len(list1) - 1
-    while y < len(list1) - 2 :
-        while u < len(list1):
-            for k in range(index1,index1+num_of_rows):
-                output[k].append(list1[u])
-            index1 += num_of_rows
-            if u+1 != len(list1) : u += 1
-            else : u = 0
-        index1 = 0
-        y += 1
-        num_of_rows = int(num_of_rows / w)
-        w -= 1
 
-    
-    return output
 
-lst = [1,2,3,4,5]
-permutations(lst)
+
+multiply_by_11("86")
