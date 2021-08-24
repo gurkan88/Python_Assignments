9c25ec1a33c678ef7299ebbec432d94434c683f0
gurkan <gurkangnr1@gmail.com>
2021-08-24 23:58:48 +0300

    permutation solution added

--- Interview questions/permutation.py
+++ Interview questions/permutation.py
@@ -1,44 +1,33 @@
-def permutations(list1):
-    factorial = 1
-    for i in range(1,len(list1)+1):
-        factorial *= i 
-    num_of_rows = int(factorial / len(list1))
-    output = []
-    for i in range(factorial):
-        output.append([])
+list1 = [1,2,3]
+factorial = 1
+for i in range(1,len(list1)+1):
+    factorial *= i 
+num_of_rows = int(factorial / len(list1))
+output = []
+for i in range(factorial):
+    output.append([])
 
-    index1 = 0
-    for u in list1:
-        for k in range(index1,index1+num_of_rows):
-            output[k].append(u)
+list2 = list1
+index1 = 0
+u = 0
+w = len(list1) - 1
+for b in range(len(list1)-1) :
+    p = 0
+    for i in range(int(len(output)/num_of_rows)):
+        y = set(list1).difference(output[index1])
+        y1 = list(y)
+        [ output[k].append(y1[0]) for k in range(index1,index1+num_of_rows)]
         index1 += num_of_rows
+        p += 1
+        y = list1
 
-    j,i = 1,0  
-    while i < factorial and j < len(list1):
-        if list1[j] != output[i][0] :
-            output[i].append(list1[j])
-            i += 1
-            if j+1 != len(list1) : j += 1
-            else: j = 0
-        else:
-            if j+1 != len(list1) : j += 1
-            else: j = 0
+    p = 0
+    index1 = 0
+    num_of_rows = int(num_of_rows / w)
+    w -= 1
 
-    for c in range(len(list1)) :
-        y = 2
-        j,i = 2,0
-        while i < factorial and j < len(list1):
-            if list1[j] not in output[i][0:y] and output[i] + [list1[j]] not in output:
-                output[i].append(list1[j])
-                y += 1
-                i += 1
-                if j+1 != len(list1) : j += 1
-                else: j = 0
-            else:
-                if j+1 != len(list1) : j += 1
-                else: j = 0
-        y += 1
-        
-    return output
-lst = [1,2,3]
-permutations(lst)
+output.sort()
+output
+
+
+
