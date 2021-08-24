6777da8f9c69c016e9d22301ed45ecececa26d3e
gurkan <gurkangnr1@gmail.com>
2021-08-25 00:41:48 +0300

    added permutation problem final version

--- Interview questions/find_permutation_final.py
+++ Interview questions/find_permutation_final.py
@@ -1,4 +1,4 @@
-list1 = [1,2,3]
+list1 = [1,2,3,4,5,6,7,8,9]
 factorial = 1
 for i in range(1,len(list1)+1):
     factorial *= i 
@@ -7,7 +7,6 @@ output = []
 for i in range(factorial):
     output.append([])
 
-list2 = list1
 index1 = 0
 u = 0
 w = len(list1) - 1
@@ -16,9 +15,10 @@ for b in range(len(list1)-1) :
     for i in range(int(len(output)/num_of_rows)):
         y = set(list1).difference(output[index1])
         y1 = list(y)
-        [ output[k].append(y1[0]) for k in range(index1,index1+num_of_rows)]
+        [ output[k].append(y1[p]) for k in range(index1,index1+num_of_rows)]
         index1 += num_of_rows
-        p += 1
+        if p+1 != len(y1) : p += 1
+        else :              p = 0
         y = list1
 
     p = 0
@@ -26,8 +26,8 @@ for b in range(len(list1)-1) :
     num_of_rows = int(num_of_rows / w)
     w -= 1
 
-output.sort()
+for q in range(len(output)):
+    diff = set(list1).difference(output[q])
+    output[q].append(*diff)
+
 output
-
-
-
