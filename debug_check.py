3ab86ea821074f9a5ab89f708ef3aef956d1ed0d
gurkan <gurkangnr1@gmail.com>
2021-08-29 22:24:58 +0300

    Minesweeper edabit challenge added

--- debug_check.py
+++ debug_check.py
@@ -1,24 +1,68 @@
-def get_lucky_number(size, nth):
-    list1 = list(range(1,size+1))
-    check_list = list1.copy()
-    filter1 = 1
-    index = filter1
-    filter_inter = list1[filter1] 
-    while True:
-        list2 = []
-        for i in range(1,len(list1)):
-            list2.append(list1[index])
-            index += filter_inter 
-            if index >= len(list1):
-                break
-        list1 = list(set(list1).difference(list2))
-        if check_list[filter1] == list1[filter1]: 
-            filter1 += 1
-            filter_inter = list1[filter1]
-        if list1[filter1] > len(list1) : 
-            break
-        index = filter_inter -1
+def num_grid(lst):
+    import copy
+    default = copy.deepcopy(lst)
+    coordinate = lst[::-1]
+    dict1 = {}
+    
+    for y in range(len(lst)):
+        for x in range(len(lst[0])):
+            coordinate[y][x] = [y,x]
+            dict1.update({"{}".format(coordinate[y][x]):"{}".format(default[y][x])})
+    dict2 = copy.deepcopy(dict1)
+    lst.insert(0,[])
+    lst.append([])
+    lst[0] = [[5,i] for i in range(-1,len(default[0])+1)]
+    lst[6] = [[-1,i] for i in range(-1,len(default[0])+1)]
+    for i,j in enumerate(range(4,-1,-1),1):
+        lst[i].append([j,5])
+    for i,j in enumerate(range(4,-1,-1),1):
+        lst[i].insert(0,[j,-1])
+    for y in lst:
+        for x in y:
+            if str(x) not in dict2.keys():
+                dict2.update({"{}".format(x):None})
+    
+    import ast
+    for i,j in dict1.items():
+        if j == "-":
+            count = 0
+            change = copy.deepcopy(i)
+            i = ast.literal_eval(i)
+            y, x = i[0], i[1]
+            i = [y, x+1]
+            for a in [y-1,y,y+1]:
+                i = [a, x+1]
+                if dict2["{}".format(i)] == "#":
+                    count += 1
+            i = [y, x]
+            for b in [y-1, y+1]:
+                i = [b, x]
+                if dict2["{}".format(i)] == "#":
+                    count += 1
+            i = [y, x-1]
+            for c in [y-1, y, y+1]:
+                i = [c, x-1]
+                if dict2["{}".format(i)] == "#":
+                    count += 1
+            dict2[change] = count
 
-    return list1
+    for i in lst:
+        for j in range(len(lst[0])):
+            if "{}".format(i[j]) in dict2.keys():
+                i[j] = dict2["{}".format(i[j])]
+    
+    for i in range(len(lst)):
+        lst[i] = list(filter(lambda x : x != None, lst[i]))
+    
+    lst = list(filter(None,lst))
+    return lst
 
-get_lucky_number(25,6)
+    
+
+num_grid([
+  ["-", "-", "-", "#", "#"],
+  ["-", "#", "-", "-", "-"],
+  ["-", "-", "#", "-", "-"],
+  ["-", "#", "#", "-", "-"],
+  ["-", "-", "-", "-", "-"]
+])
